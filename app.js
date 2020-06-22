f421a636ec88f2038d6c42149fa7da42cf965432
Paul Choi <paulsoonminchoi@gmail.com>
2020-06-21 21:40:31 -0700

    added two new versions

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log('hello world')
+//get the button attribute 
+const submitBtn = document.querySelector(".submit-btn")
+
+submitBtn.addEventListener("click", function () {
+    console.log('hello world')
+});
